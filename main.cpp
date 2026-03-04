cc793c90282d0ab3bcdc609f39d2483af4273912
Jakub Jasiński <jjasinski1209@gmail.com>
2026-03-04 09:49:41 +0100

    Initial main file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include <random>
-
+#include <cstdlib>
+#include <vector>
 
 int main(){
+  srand(time(0));
+  
+  int n = 0;
 
+  std::cout<<"n: ";
+  std::cin>>n;
+
+  for(int i = 0; i < n; i++){
+
+  }
 
   return 0;
 }
