9bba8864c6a2bc11e8ddca258f8874561958227d
Jakub Jasiński <jjasinski1209@gmail.com>
2026-03-04 09:58:27 +0100

    Generator

--- main.cpp
+++ main.cpp
@@ -9,10 +9,29 @@ int main(){
 
   std::cout<<"n: ";
   std::cin>>n;
+  std::vector<int> pj;
 
   for(int i = 0; i < n; i++){
+    pj.push_back((rand() % 29) + 1);
+  }
+
+  int A = 0;
+  for(int i = 0; i < n; i++){
+    A += pj[i];
+  }
+
+  std::vector<int> rj;
+
+  for(int i = 0; i < n; i++){
+      
+    rj.push_back((rand() % A) + 1);
 
   }
 
+  for(int i = 0; i < n; i++){
+    std::cout<<"pj: "<<pj[i]<<" ";
+    std::cout<<"rj: "<<rj[i]<<std::endl;
+  }
+  std::cout<<"A: "<<A<<std::endl;
   return 0;
 }
